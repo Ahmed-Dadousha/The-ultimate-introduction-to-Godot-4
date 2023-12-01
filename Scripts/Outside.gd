4f4fb5ac2e1380cb919f44be9587733a89c2c0f7
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2023-12-01 19:35:07 +0200

    I learned about User Interface

--- Scripts/Outside.gd
+++ Scripts/Outside.gd
@@ -4,7 +4,7 @@ class_name ItemContainer
 func _on_gate_player_entered_gate():
 	var tween = create_tween()
 	tween.tween_property($player,"speed",0,.5)
-	#
 	call_deferred("teleport")
+	
 func teleport():
 	get_tree().change_scene_to_file("res://Scenes/Inside.tscn")
