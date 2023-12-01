4dba4b2e7d21685734595c182c09b8124725f79c
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2023-12-01 18:59:50 +0200

    Fixed: level transtion

--- Scripts/Inside.gd
+++ Scripts/Inside.gd
@@ -1,9 +1,10 @@
 extends LevelParnet
 
 
-
-
 func _on_exit_game_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($player,"speed",0,0.5)
+	call_deferred("teleport")
+	
+func teleport():	
 	get_tree().change_scene_to_file("res://Scenes/Outside.tscn")
