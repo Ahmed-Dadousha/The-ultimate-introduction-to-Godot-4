85c70045bd1813db1510c0caee45db437d2c3467
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2023-12-01 18:44:54 +0200

    I Learned level transtion

--- Scripts/Inside.gd
+++ Scripts/Inside.gd
@@ -3,6 +3,7 @@ extends LevelParnet
 
 
 
-func _on_exit_game_area_body_entered(body):
+func _on_exit_game_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($player,"speed",0,0.5)
+	get_tree().change_scene_to_file("res://Scenes/Outside.tscn")
--- Scripts/Outside.gd
+++ Scripts/Outside.gd
@@ -4,3 +4,4 @@ class_name ItemContainer
 func _on_gate_player_entered_gate():
 	var tween = create_tween()
 	tween.tween_property($player,"speed",0,.5)
+	get_tree().change_scene_to_file("res://Scenes/Inside.tscn")
